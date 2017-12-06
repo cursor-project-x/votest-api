5933fda290d783f299fd1a27d971ee291f50f7ab
MVolodya <medvid.volodya@gmail.com>
2017-12-06 18:33:11 +0200

    #13 added folder for webpack configuration

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "[name].bundle.js",
     libraryTarget: "commonjs2"
   }
